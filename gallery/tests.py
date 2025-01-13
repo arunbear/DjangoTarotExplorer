0a986f1498db2d8c94d4f838b45a5bef083628d9
arunp <arunbear@cpan.org>
2025-01-13 16:14:23 +0000

    Check that gallery contains a table

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,6 +1,13 @@
+from bs4 import BeautifulSoup
 from django.test import TestCase
 
 class GalleryIndexViewSpecs(TestCase):
     def test_that_gallery_url_exists(self):
         response = self.client.get("/gallery/")
         self.assertIs(response.status_code, 200)
+
+    def test_that_gallery_contains_table(self):
+        response = self.client.get("/gallery/")
+        soup = BeautifulSoup(response.content, features="html.parser")
+        table = soup.find("table", {"id": "gallery"})
+        self.assertIsNotNone(table)
