bdb2db2fe37916c71cfcc02c62ed23c86ac49d91
arunp <arunbear@cpan.org>
2025-01-16 13:57:41 +0000

    Add allowed hosts for deployment

--- tarotexplorer/settings.py
+++ tarotexplorer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0l$(i0k=bdb#hm-+j$vl(e^qsu*%8wprbqm1x1zd+^d90y0@^f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'arunbear.eu.pythonanywhere.com']
 
 
 # Application definition
