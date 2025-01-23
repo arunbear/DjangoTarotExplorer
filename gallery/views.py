4f174bc4ca3e1cac56d789aee37d75bb47081c32
arunp <arunbear@cpan.org>
2025-01-23 19:54:54 +0000

    Improve template naming

--- gallery/views.py
+++ gallery/views.py
@@ -9,10 +9,10 @@ def index(request):
     return render(request, 'gallery/index.html', context={'cards': deck.all_cards()})
 
 def pips(request):
-    return render(request, 'gallery/royals.html', context={'cards': deck.pips()})
+    return render(request, 'gallery/4_col_grid.html', context={'cards': deck.pips()})
 
 def royals(request):
-    return render(request, 'gallery/royals.html', { 'cards': gallery.models.royals.by_suite })
+    return render(request, 'gallery/4_col_grid.html', {'cards': gallery.models.royals.by_suite})
 
 def royals_by_rank(request):
-    return render(request, 'gallery/royals.html', { 'cards': gallery.models.royals.by_rank })
+    return render(request, 'gallery/4_col_grid.html', {'cards': gallery.models.royals.by_rank})
